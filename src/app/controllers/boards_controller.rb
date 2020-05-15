dc3f4c9d0c28cd916f6dd515b689ddd090dd351a
remon2929 <ff.remon.9123@gmail.com>
2020-05-15 13:35:44 +0900

    ページネーションの実装

--- src/app/controllers/boards_controller.rb
+++ src/app/controllers/boards_controller.rb
@@ -1,11 +1,13 @@
 class BoardsController < ApplicationController
-  def index
-    @boards = Board.all
-  end
+  before_action :set_target_board, only: %i[show edit update destroy]
 
-  def new
+def index
+    @boards = Board.page(params[:page])
+end
+
+def new
     @board = Board.new
-  end
+end
 
 def create
 board = Board.create(board_params)
@@ -14,22 +16,19 @@ redirect_to board
 end
 
 def show
-  @board = Board.find(params[:id])
+  
 end
 
 def edit
-  @board = Board.find(params[:id])
+
 end
 
 def update
-  board = Board.find(params[:id])
   board.update(board_params)
-
   redirect_to board
 end
 
 def destroy
-  board = Board.find(params[:id])
   board.delete
 
   redirect_to boards_path
@@ -38,6 +37,10 @@ end
 private 
 def board_params
 params.require(:board).permit(:name, :title, :body)
+end
+
+def set_target_board
+  @board = Board.find(params[:id])
 end
 
 end
