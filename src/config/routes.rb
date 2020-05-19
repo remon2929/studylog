002770dd92d2440a23e2f573301b4d03eda2419c
remon2929 <ff.remon.9123@gmail.com>
2020-05-19 11:30:18 +0900

    認証機能の設定

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get 'mypage', to: 'users#me'
+post 'login' ,  to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
+get 'sessions/create'
+get 'sessions/destroy'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'home#index'
+resources: users, only: %i[new create]
 resources :boards
 resources :comments, only: %i[create destroy]
 
