2fdcdb67a59f63bd8af26d1fc37b6fd800f9ced8
remon2929 <ff.remon.9123@gmail.com>
2020-05-22 18:29:55 +0900

    ユーザー登録フォームの実装

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -7,7 +7,7 @@ get 'sessions/create'
 get 'sessions/destroy'
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'home#index'
-resources: users, only: %i[new create]
+resources :users, only: %i[new create]
 resources :boards
 resources :comments, only: %i[create destroy]
 
